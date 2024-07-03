20749437dab733137d5d42ce835bc26e735b2a10
Dhiraj-Gawali <dhirajgawali22@gmail.com>
2024-07-03 09:55:39 +0530

    Google Add Place API with cucumber

---
+++ src/main/java/pojo/GooglePlaceAPI/Location.java
@@ -0,0 +1,26 @@
+package pojo.GooglePlaceAPI;
+
+public class Location {
+    private double lat;
+    private double lng;
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+}
+/*{
+    "lat": -38.383494,
+    "lng": 33.427362
+  }*/
--- src/test/java/feature/placeValidation.feature
+++ src/test/java/feature/placeValidation.feature
@@ -3,5 +3,5 @@ Feature: Validate the Google place API's
   Scenario: Validate the Add place API
     Given Add place API payload
     When User calls the add place API with post request
-    Then The API call should be success with the error code
-    And Response should be OK
+    Then "status" should be "OK"
+    And "scope" should be "APP"
