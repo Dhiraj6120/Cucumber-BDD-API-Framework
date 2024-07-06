d47a4759122d65b96028eb5788e9d708ede827e1
Dhiraj-Gawali <dhirajgawali22@gmail.com>
2024-07-06 22:43:48 +0530

    Code Refactoring

--- src/test/java/Resources/GetTestData.java
+++ src/test/java/Resources/GetTestData.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class GetTestData extends Utils{
     public GoogleAddPlaceAPIRequest addPlacePayload(String name, String language, String address){
         try {
-            RestAssured.baseURI = getProp().getProperty("baseUrl");
+            RestAssured.baseURI = getGlobalValue("baseUrl");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
